2c0810d7aaf0aa16cad65538cf49d82374a28e1a
Srikant Loric <gandukprasad@gmail.com>
2023-05-27 11:09:31 +0530

    commit

---
+++ index.js.js
@@ -0,0 +1,54 @@
+var express = require("express")
+const compute = require('@google-cloud/compute');
+const {InstancesClient} = require('@google-cloud/compute').v1;
+
+const port = process.env.PORT || 3001
+
+var app = express()
+
+
+
+
+
+
+app.get("/mainAPI", (req, res, next) => {
+
+
+    
+    const instanceID = '' +req.query.instance;
+    const zoneID = ''+req.query.zone
+   
+
+  const computeClient = new InstancesClient();
+
+  async function callStart() {
+    const request = {
+      instance:instanceID,
+      project:'fabled-frame-383008',
+      zone:zoneID,
+    };
+
+    // Run request
+    const response = await computeClient.get(request)
+    // console.log(response[0])
+      res.send(JSON.stringify(
+          {data:response}
+      ))
+  }
+
+  callStart();
+}
+)
+
+app.get("/test", (req,res) => {
+  res.send("working")
+})
+
+
+app.listen(port, () => {
+  console.log("listening at port :3001");
+})
+
+
+
+
